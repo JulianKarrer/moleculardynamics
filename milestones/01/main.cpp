58b7614e4ef6215bcd542d76475be7de987640d5
Lucas Frérot <lucas.frerot@gmail.com>
2022-08-03 18:20:13 +0200

    renamed verlet to maintain file creation in milestone 02

--- milestones/01/main.cpp
+++ milestones/01/main.cpp
@@ -1,4 +1,4 @@
-#include "verlet.h"
+#include "hello.h"
 #include <Eigen/Dense>
 #include <iostream>
 
@@ -6,7 +6,6 @@
 #include <mpi.h>
 #endif
 
-using Eigen::MatrixXd;
 
 int main(int argc, char *argv[]) {
     int rank = 0, size = 1;
@@ -22,25 +21,8 @@ int main(int argc, char *argv[]) {
 
     std::cout << "Hello I am rank " << rank << " of " << size << "\n";
 
-    // Below is some Eigen code
-    MatrixXd m(2, 2);
-    m(0, 0) = 3;
-    m(1, 0) = 2.5;
-    m(0, 1) = -1;
-    m(1, 1) = m(1, 0) + m(0, 1);
-
-    // C++11 style loop over elements of an Eigen matrix
-    for (auto&& value : m.reshaped())
-        value += 1.;
-
-    // Output matrix values
     if (rank == 0)
-        std::cout << m << std::endl;
-
-    // Below is some code from our MD library
-    double x, y, z, vx, vy, vz;
-    scalar_verlet_step1(x, y, z, vx, vy, vz, 0, 0, 0, 1);
-    scalar_verlet_step2(vx, vy, vz, 0, 0, 0, 1);
+      hello_eigen();
 
 #ifdef USE_MPI
     MPI_Finalize();
