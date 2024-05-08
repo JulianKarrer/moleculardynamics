e384906e421be2b55d010d0645aa5e25e460570a
Julian Karrer <juliankarrer@t-online.de>
2024-05-08 18:54:19 +0200

    Fix atoms implementation

--- src/atoms.cpp
+++ src/atoms.cpp
@@ -1,28 +1,21 @@
 #include "atoms.h"
 
-class Atoms {
-  public:
-    Positions_t positions;
-    Velocities_t velocities;
-    Forces_t forces;
+Atoms::Atoms(const Positions_t &p)
+    : positions{p},
+      velocities{3, p.cols()},
+      forces{3, p.cols()} {
+    velocities.setZero();
+    forces.setZero();
+};
 
-    Atoms::Atoms(const Positions_t &p)
-        : positions{p},
-          velocities{3, p.cols()},
-          forces{3, p.cols()} {
-        velocities.setZero();
-        forces.setZero();
-    }
+Atoms::Atoms(const Positions_t &p, const Velocities_t &v)
+    : positions{p},
+      velocities{v},
+      forces{3, p.cols()} {
+    assert(p.cols() == v.cols());
+    forces.setZero();
+};
 
-    Atoms(const Positions_t &p, const Velocities_t &v)
-        : positions{p},
-          velocities{v},
-          forces{3, p.cols()} {
-        assert(p.cols() == v.cols());
-        forces.setZero();
-    }
-
-    size_t nb_atoms() const {
-        return positions.cols();
-    }
+size_t Atoms::nb_atoms() const {
+    return positions.cols();
 };
